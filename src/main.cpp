32e39989fb0487b8c6284b8d499d367e4d26f4e4
LEAVING-7 <leavingqwq@gmail.com>
2026-06-10 14:15:20 +0800

    simplify example

--- src/main.cpp
+++ src/main.cpp
@@ -2,49 +2,24 @@
 
 #include <ui.hpp>
 
-struct AppContext {
-  ui::Window *mainwin;
+struct GalleryApp {
+  ui::Spinbox spinbox;
+  ui::Slider slider;
+  ui::ProgressBar progress;
+
+  ui::Button open_file_button;
+  ui::Entry open_file_entry;
+  ui::Button open_folder_button;
+  ui::Entry open_folder_entry;
+  ui::Button save_file_button;
+  ui::Entry save_file_entry;
+  ui::Button msg_box_button;
+  ui::Button error_box_button;
+
+  ui::Window *mainwin = nullptr;
 };
 
-static ui::Spinbox g_spinbox;
-static ui::Slider g_slider;
-static ui::ProgressBar g_progress;
-
-static ui::Button g_open_file_button;
-static ui::Entry g_open_file_entry;
-static ui::Button g_open_folder_button;
-static ui::Entry g_open_folder_entry;
-static ui::Button g_save_file_button;
-static ui::Entry g_save_file_entry;
-static ui::Button g_msg_box_button;
-static ui::Button g_error_box_button;
-
-static ui::Window *g_mainwin;
-
-static int on_closing(uiWindow *sender, void *data) {
-  (void)sender;
-  ui::Application::quit();
-  return 1;
-}
-
-static int on_should_quit(void *data) {
-  static_cast<AppContext *>(data)->mainwin->destroy();
-  return 1;
-}
-
-static void on_spinbox_changed(uiSpinbox *sender, void *) {
-  (void)sender;
-  int value = g_spinbox.value();
-  g_slider.set_value(value);
-  g_progress.set_value(value);
-}
-
-static void on_slider_changed(uiSlider *sender, void *) {
-  (void)sender;
-  int value = g_slider.value();
-  g_spinbox.set_value(value);
-  g_progress.set_value(value);
-}
+static GalleryApp g_app;
 
 static ui::VerticalBox make_basic_controls_page() {
   return ui::VerticalBox::make(
@@ -66,10 +41,26 @@ static ui::VerticalBox make_basic_controls_page() {
 static ui::HorizontalBox make_numbers_page() {
   return ui::HorizontalBox::make()
       .append_stretchy(ui::Group::make("Numbers").margined(true).set_child(
-          ui::VerticalBox::make(
-              ui::Spinbox::make_into(g_spinbox, 0, 100).on_changed(on_spinbox_changed, nullptr),
-              ui::Slider::make_into(g_slider, 0, 100).on_changed(on_slider_changed, nullptr),
-              ui::ProgressBar::make_into(g_progress), ui::ProgressBar::make().set_value(-1))
+          ui::VerticalBox::make(ui::Spinbox::make_into(g_app.spinbox, 0, 100)
+                                    .on_changed(
+                                        [](uiSpinbox *sender, void *) {
+                                          (void)sender;
+                                          int value = g_app.spinbox.value();
+                                          g_app.slider.set_value(value);
+                                          g_app.progress.set_value(value);
+                                        },
+                                        nullptr),
+                                ui::Slider::make_into(g_app.slider, 0, 100)
+                                    .on_changed(
+                                        [](uiSlider *sender, void *) {
+                                          (void)sender;
+                                          int value = g_app.slider.value();
+                                          g_app.spinbox.set_value(value);
+                                          g_app.progress.set_value(value);
+                                        },
+                                        nullptr),
+                                ui::ProgressBar::make_into(g_app.progress),
+                                ui::ProgressBar::make().set_value(-1))
               .padded(true)))
       .append_stretchy(ui::Group::make("Lists").margined(true).set_child(
           ui::VerticalBox::make(ui::Combobox::make()
@@ -88,56 +79,6 @@ static ui::HorizontalBox make_numbers_page() {
       .padded(true);
 }
 
-static void on_open_file_clicked(uiButton *sender, void *data) {
-  (void)sender;
-  ui::Entry *entry = static_cast<ui::Entry *>(data);
-  printf("sender: %p, data: %p, entry_raw: %p\n", sender, data, entry->raw());
-
-  ui::Text filename = ui::open_file(*g_mainwin);
-  if (filename.empty()) {
-    entry->set_text("(cancelled)");
-    return;
-  }
-  entry->set_text(filename.c_str());
-}
-
-static void on_open_folder_clicked(uiButton *sender, void *data) {
-  (void)sender;
-  printf("sender: %p, data: %p\n", sender, data);
-
-  ui::Entry *entry = static_cast<ui::Entry *>(data);
-  ui::Text filename = ui::open_folder(*g_mainwin);
-  if (filename.empty()) {
-    entry->set_text("(cancelled)");
-    return;
-  }
-  entry->set_text(filename.c_str());
-}
-
-static void on_save_file_clicked(uiButton *sender, void *data) {
-  (void)sender;
-  printf("sender: %p, data: %p\n", sender, data);
-  ui::Entry *entry = static_cast<ui::Entry *>(data);
-  ui::Text filename = ui::save_file(*g_mainwin);
-  if (filename.empty()) {
-    entry->set_text("(cancelled)");
-    return;
-  }
-  entry->set_text(filename.c_str());
-}
-
-static void on_msg_box_clicked(uiButton *sender, void *) {
-  (void)sender;
-  ui::msg_box(*g_mainwin, "This is a normal message box.",
-              "More detailed information can be shown here.");
-}
-
-static void on_msg_box_error_clicked(uiButton *sender, void *) {
-  (void)sender;
-  ui::msg_box_error(*g_mainwin, "This message box describes an error.",
-                    "More detailed information can be shown here.");
-}
-
 static ui::HorizontalBox make_data_choosers_page() {
   return ui::HorizontalBox::make(
              ui::VerticalBox::make(ui::DateTimePicker::make_date(), ui::DateTimePicker::make_time(),
@@ -149,28 +90,77 @@ static ui::HorizontalBox make_data_choosers_page() {
           ui::VerticalBox::make(
               ui::Grid::make()
                   .padded(true)
-                  .append(ui::Entry::make_into(g_open_file_entry).readonly(true), 1, 0, 1, 1, true,
-                          uiAlignFill, false, uiAlignFill)
-                  .append(ui::Button::make_into(g_open_file_button, "  Open File  ")
-                              .on_clicked(on_open_file_clicked, &g_open_file_entry),
-                          0, 0, 1, 1, false, uiAlignFill, false, uiAlignFill)
-                  .append(ui::Entry::make_into(g_open_folder_entry).readonly(true), 1, 1, 1, 1,
+                  .append(ui::Entry::make_into(g_app.open_file_entry).readonly(true), 1, 0, 1, 1,
                           true, uiAlignFill, false, uiAlignFill)
-                  .append(ui::Button::make_into(g_open_folder_button, "Open Folder")
-                              .on_clicked(on_open_folder_clicked, &g_open_folder_entry),
+                  .append(ui::Button::make_into(g_app.open_file_button, "  Open File  ")
+                              .on_clicked(
+                                  [](uiButton *sender, void *data) {
+                                    ui::Entry *entry = static_cast<ui::Entry *>(data);
+                                    printf("sender: %p, data: %p, entry_raw: %p\n", sender, data,
+                                           entry->raw());
+                                    ui::Text filename = ui::open_file(*g_app.mainwin);
+                                    if (filename.empty()) {
+                                      entry->set_text("(cancelled)");
+                                      return;
+                                    }
+                                    entry->set_text(filename.c_str());
+                                  },
+                                  &g_app.open_file_entry),
+                          0, 0, 1, 1, false, uiAlignFill, false, uiAlignFill)
+                  .append(ui::Entry::make_into(g_app.open_folder_entry).readonly(true), 1, 1, 1, 1,
+                          true, uiAlignFill, false, uiAlignFill)
+                  .append(ui::Button::make_into(g_app.open_folder_button, "Open Folder")
+                              .on_clicked(
+                                  [](uiButton *sender, void *data) {
+                                    printf("sender: %p, data: %p\n", sender, data);
+                                    ui::Entry *entry = static_cast<ui::Entry *>(data);
+                                    ui::Text filename = ui::open_folder(*g_app.mainwin);
+                                    if (filename.empty()) {
+                                      entry->set_text("(cancelled)");
+                                      return;
+                                    }
+                                    entry->set_text(filename.c_str());
+                                  },
+                                  &g_app.open_folder_entry),
                           0, 1, 1, 1, false, uiAlignFill, false, uiAlignFill)
-                  .append(ui::Entry::make_into(g_save_file_entry).readonly(true), 1, 2, 1, 1, true,
-                          uiAlignFill, false, uiAlignFill)
-                  .append(ui::Button::make_into(g_save_file_button, "  Save File  ")
-                              .on_clicked(on_save_file_clicked, &g_save_file_entry),
+                  .append(ui::Entry::make_into(g_app.save_file_entry).readonly(true), 1, 2, 1, 1,
+                          true, uiAlignFill, false, uiAlignFill)
+                  .append(ui::Button::make_into(g_app.save_file_button, "  Save File  ")
+                              .on_clicked(
+                                  [](uiButton *sender, void *data) {
+                                    (void)sender;
+                                    ui::Entry *entry = static_cast<ui::Entry *>(data);
+                                    ui::Text filename = ui::save_file(*g_app.mainwin);
+                                    if (filename.empty()) {
+                                      entry->set_text("(cancelled)");
+                                      return;
+                                    }
+                                    entry->set_text(filename.c_str());
+                                  },
+                                  &g_app.save_file_entry),
                           0, 2, 1, 1, false, uiAlignFill, false, uiAlignFill)
                   .append(ui::Grid::make()
                               .padded(true)
-                              .append(ui::Button::make_into(g_msg_box_button, "Message Box")
-                                          .on_clicked(on_msg_box_clicked, nullptr),
+                              .append(ui::Button::make_into(g_app.msg_box_button, "Message Box")
+                                          .on_clicked(
+                                              [](uiButton *sender, void *) {
+                                                (void)sender;
+                                                ui::msg_box(
+                                                    *g_app.mainwin, "This is a normal message box.",
+                                                    "More detailed information can be shown here.");
+                                              },
+                                              nullptr),
                                       0, 0, 1, 1, false, uiAlignFill, false, uiAlignFill)
-                              .append(ui::Button::make_into(g_error_box_button, "Error Box")
-                                          .on_clicked(on_msg_box_error_clicked, nullptr),
+                              .append(ui::Button::make_into(g_app.error_box_button, "Error Box")
+                                          .on_clicked(
+                                              [](uiButton *sender, void *) {
+                                                (void)sender;
+                                                ui::msg_box_error(
+                                                    *g_app.mainwin,
+                                                    "This message box describes an error.",
+                                                    "More detailed information can be shown here.");
+                                              },
+                                              nullptr),
                                       1, 0, 1, 1, false, uiAlignFill, false, uiAlignFill),
                           0, 3, 2, 1, false, uiAlignCenter, false, uiAlignStart))
               .padded(true))
@@ -186,11 +176,20 @@ int main() {
 
   {
     ui::Window mainwin = ui::Window::make("libui Control Gallery", 640, 480, true);
-    g_mainwin = &mainwin;
+    g_app.mainwin = &mainwin;
 
-    AppContext ctx = {&mainwin};
-    mainwin.on_closing(on_closing, &ctx);
-    ui::Application::on_should_quit(on_should_quit, &ctx);
+    mainwin.on_closing(
+        [](uiWindow *, void *) {
+          ui::Application::quit();
+          return 1;
+        },
+        nullptr);
+    ui::Application::on_should_quit(
+        [](void *) {
+          g_app.mainwin->destroy();
+          return 1;
+        },
+        nullptr);
 
     ui::Tab tab = ui::Tab::make();
     mainwin.set_child(tab);
